728407587af05721d6367eae3e647e86fd7af87b
Valentin_Tsushko <valentin.tsushko95@gmail.com>
2021-12-10 13:57:02 +0200

    'Version_0.1'

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo1.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h4>Hi there.</h4>
+        </p>
+        <a
+          className="App-link"
+          href="https://polinomnewton1.000webhostapp.com/"
+          rel="noopener noreferrer"
+        >
+            <h1>Hello, world!</h1>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
